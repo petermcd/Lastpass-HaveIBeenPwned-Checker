822e1de595cb7081e2569db0669bf6d607bc16cf
Peter McDonald <pete@petermcdonald.co.uk>
2019-01-18 23:24:04 +0000

    Add project files.

---
+++ Lastpass HaveIBeenPwned Checker/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lastpass_HaveIBeenPwned_Checker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
